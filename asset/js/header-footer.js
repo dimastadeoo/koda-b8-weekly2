519061870ae9c8dc8fe38d2051b97ce3728038d7
dimastadeoo <dimastadeoo@gmail.com>
2026-06-10 16:47:15 +0700

    feat: create a hamburger menu on the navbar in header.html and add js logic to display the hamburger menu

--- asset/js/header-footer.js
+++ asset/js/header-footer.js
@@ -1,13 +1,23 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch('/component/header.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('header').innerHTML = data;
-        });
+async function display(componen, id) {
+    const respon = await fetch(componen)
+    document.getElementById(id).innerHTML = await respon.text()
+}
 
-    fetch('/component/footer.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('footer').innerHTML = data;
-        });
-});
+async function main() {
+
+    const header = await display('/component/header.html', 'header')
+    const footer = await display('/component/footer.html', 'footer')
+    
+    //humberger navbar
+    const menuBtn = document.getElementById('menu-btn');
+    const navMenu = document.getElementById('nav-menu');
+    const arrowIcon = document.getElementById('arrow-icon');
+
+    menuBtn.addEventListener('click', function () {
+        navMenu.classList.toggle('hidden');
+
+        arrowIcon.classList.toggle('rotate-180');
+    });
+}
+
+main()
